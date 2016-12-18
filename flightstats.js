33af0b550590964ed878143c88dd3d604b896f9e
Laura Fiig <Laura Fiig>
2016-12-17 21:06:58 -0600

    find an airport/airline function cleaned up

--- flightstats.js
+++ flightstats.js
@@ -1,108 +1,91 @@
-//where do you want to fly
-//from to
-//what date and time
-//ontime status of flight/airprot
-//weather forecast
-//airport info and alert
 
 $(document).ready(function() {
 	// keys
 	var fSappId = "3bb6e9e5"
 	var fSappKey = "725fa65bd5d71ecd80d2acd493e4f562"
 	
-	//when submit button is clicked
-	$("#airline").on("click", function() {  
-		// build query
+	//when airline button is clicked
+	$("#airline").on("click", function() {
+		// clear list
 		$("#list").html("")
-		console.log("airlines!!")
+		// capture input, set case
 		var input = $("#input").val().toLowerCase()
+		//check for no input
 		if (input != "") {
-			console.log("you asked for: " + input)
+			// build query
 			var queryAirline =  "https://api.flightstats.com/flex/airlines/rest/v1/json/active?appId="+fSappId+"&appKey="+fSappKey
 			console.log(queryAirline)
 			// get data from api
 			$.ajax({url: queryAirline, method: 'GET'})
 			//create object
 			.done(function(aLResponse) {
-				// look at data
-				console.log(aLResponse)
-				console.log("a.l"+aLResponse.airlines.length)
+				// loop thru data
 				for (i=0; i<aLResponse.airlines.length; i++){
+					// get name from response, set case, check for match to user input
 					var name = aLResponse.airlines[i].name.toLowerCase()
 					var check = name.search(input);
 					if (check != -1) {
-						console.log("XXXXXXXXXXXXXXX")
-						console.log([i]+": "+aLResponse.airlines[i].name);
-						console.log([i]+": "+aLResponse.airlines[i].fs);
-						console.log("XXXXXXXXXXXXXXX")
+						//if match, add to table
 						$("#choice").html("Airline")
-						$("#list").append("<tr><td>"+aLResponse.airlines[i].name+"</td><td>"+aLResponse.airlines[i].fs+"</td></tr>")						
-					}
-				}
-				});  // end .done
+						$("#list").append("<tr><td>"+aLResponse.airlines[i].name+"</td><td>"+aLResponse.airlines[i].fs+"</td></tr>")
+					} //end if check
+				}  // end for loop
+			});  // end .done
 			return false;
-		}; // end if
+		}; // end if input
 	}); // end on click
 
-	//when submit button is clicked
-	$("#airport").on("click", function() {  
-		// build query
+	//when airport button is clicked
+	$("#airport").on("click", function() {
+		// clear list
 		$("#list").html("")
-		console.log("airports!!")
+		// capture input, set case
 		var input = $("#input").val().toLowerCase()
+		//check for no input
 		if (input != "") {
-			console.log("you asked for: " + input)	
+			// build query
 			var queryAirport = "https://api.flightstats.com/flex/airports/rest/v1/json/active?appId="+fSappId+"&appKey="+fSappKey
 			console.log(queryAirport)
-			// get data from api
+			// get data
 			$.ajax({url: queryAirport, method: 'GET'})
 			//create object
 			.done(function(aPResponse) {
-				// look at data
-				console.log(aPResponse)
-				console.log("a.l"+aPResponse.airports.length)
+				// loop thru data
 				for (i=0; i<aPResponse.airports.length; i++){
+					// get name from response, set case, check for match to user input
 					var name = aPResponse.airports[i].name.toLowerCase()
 					var check = name.search(input);
 					if (check != -1) {
-						console.log("XXXXXXXXXXXXXXX")
-						console.log([i]+": "+aPResponse.airports[i].name);
-						console.log([i]+": "+aPResponse.airports[i].fs);
-						console.log("XXXXXXXXXXXXXXX")
+						//if match, add to table
 						$("#choice").html("Airport")
 						$("#list").append("<tr><td>"+aPResponse.airports[i].name+"</td><td>"+aPResponse.airports[i].fs+"</td></tr>")
-					}
-				}
+					} //end if check
+				}  // end for loop
 			});  // end .done
 			return false;
-		}; // end if	
+		}; // end if input
 	}); // end on click
 
 /*
-	//when submit button is clicked
-	$("#enter").on("click", function() {  
-		//read value from form
-		var location = $("#location").val();
-		//check results
-		console.log("loc: "+ location);
-		// build query
-		var queryGeoURL = "https://maps.googleapis.com/maps/api/geocode/json?address="+location+"&key=" + myKey
-		console.log(queryGeoURL)
-		// get data from api
-		$.ajax({url: queryGeoURL, method: 'GET'})
-		//create object
-		.done(function(geoResponse) {
-			// look at data
-			console.log(geoResponse)
-			// put in variable
-			var lat = geoResponse.results[0].geometry.location.lat;
-			var lng = geoResponse.results[0].geometry.location.lng;
-			// check varaibles
-			console.log(lat+","+lng);
-		});  // end .done
-		return false;
-	}); // end on click
+console.log("airlines!!")
+console.log("you asked for: " + input)
+console.log(queryAirline)
+console.log(aLResponse)
+console.log("a.l"+aLResponse.airlines.length)
+console.log("XXXXXXXXXXXXXXX")
+console.log([i]+": "+aLResponse.airlines[i].name);
+console.log([i]+": "+aLResponse.airlines[i].fs);
+console.log("XXXXXXXXXXXXXXX")
+
+console.log("airports!!")
+console.log("you asked for: " + input)	
+console.log(queryAirport)
+console.log(aPResponse)
+console.log("a.l"+aPResponse.airports.length)
+console.log("XXXXXXXXXXXXXXX")
+console.log([i]+": "+aPResponse.airports[i].name);
+console.log([i]+": "+aPResponse.airports[i].fs);
+console.log("XXXXXXXXXXXXXXX")
 */
 
 });  // end document.ready
-
