e38c7aea394de8336d6c7067920fd3db8427ae6a
Laura Fiig <Laura Fiig>
2016-12-21 20:54:02 -0600

    table

---
+++ schema.sql
@@ -0,0 +1,22 @@
+CREATE database travelApp_db;
+
+USE travelApp_db;
+CREATE TABLE destinations (
+	id INTEGER AUTO_INCREMENT NOT NULL,
+	PRIMARY KEY (id),
+	destination VARCHAR(100) NOT NULL,
+	themeParks INTEGER NOT NULL,
+	roadTrip INTEGER NOT NULL,
+	plan INTEGER NOT NULL,
+	crowds INTEGER NOT NULL,
+	warm INTEGER NOT NULL,
+	sports INTEGER NOT NULL,
+	outdoor INTEGER NOT NULL,
+	water INTEGER NOT NULL,
+	cities INTEGER NOT NULL,
+	historic INTEGER NOT NULL,
+	date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+)
+
+
+
---
+++ seeds.sql
@@ -0,0 +1,19 @@
+USE travelApp_db;
+INSERT INTO destinations (destination, themeParks, roadTrip, plan, crowds, warm, sports, outdoor, water, cities, historic)
+VALUES ("New York City",	2,	1,	3,	5,	3,	5,	2,	2,	5,	4),
+("New Orleans",				1,	2,	2,	5,	5,	3,	1,	1,	5,	4),
+("Los Angeles",				4,	1,	4,	5,	5,	4,	1,	3,	4,	2),
+("Washington DC",			2,	1,	4,	5,	3,	2,	2,	1,	5,	5),
+("San Francisco",			3,	3,	3,	5,	2,	2,	2,	2,	5,	3),
+("Las Vegas",				4,	3,	3,	5,	4,	1,	1,	1,	3,	1),
+("Oahu",					1,	2,	4,	2,	5,	2,	4,	5,	5,	2),
+("Maui",					1,	3,	3,	1,	5,	2,	5,	5,	1,	1),
+("Disney",					5,	3,	5,	5,	5,	1,	3,	3,	1,	1),
+("Florida Keys",			1,	4,	2,	3,	5,	2,	4,	5,	1,	1),
+("Central Texas",		 	2,	4,	3,	3,	4,	5,	5,	4,	3,	5),
+("Mount Rushmore",			1,	5,	2,	1,	2,	2,	5,	1,	1,	3),
+("Grand Canyon",			1,	5,	2,	2,	4,	2,	5,	1,	1,	1),
+("Vermont",					1,	4,	3,	3,	1,	4,	5,	1,	1,	2),
+("Glacier National Park",	2,	5,	3,	1,	1,	2,	5,	1,	1,	1),
+("Aspen",					1,	3,	3,	3,	3,	3,	5,	1,	1,	1)
+
