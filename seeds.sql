5158b8c72327972d278559fc328ae61dd2e2d640
Laura Fiig <Laura Fiig>
2016-12-22 22:49:11 -0600

    photo lonks added

--- seeds.sql
+++ seeds.sql
@@ -1,19 +1,19 @@
 USE travelApp_db;
-INSERT INTO destinations (destination, themeParks, roadTrip, plan, crowds, warm, sports, outdoor, water, cities, historic)
-VALUES ("New York City",	2,	1,	3,	5,	3,	5,	2,	2,	5,	4),
-("New Orleans",				1,	2,	2,	5,	5,	3,	1,	1,	5,	4),
-("Los Angeles",				4,	1,	4,	5,	5,	4,	1,	3,	4,	2),
-("Washington DC",			2,	1,	4,	5,	3,	2,	2,	1,	5,	5),
-("San Francisco",			3,	3,	3,	5,	2,	2,	2,	2,	5,	3),
-("Las Vegas",				4,	3,	3,	5,	4,	1,	1,	1,	3,	1),
-("Oahu",					1,	2,	4,	2,	5,	2,	4,	5,	5,	2),
-("Maui",					1,	3,	3,	1,	5,	2,	5,	5,	1,	1),
-("Disney",					5,	3,	5,	5,	5,	1,	3,	3,	1,	1),
-("Florida Keys",			1,	4,	2,	3,	5,	2,	4,	5,	1,	1),
-("Central Texas",		 	2,	4,	3,	3,	4,	5,	5,	4,	3,	5),
-("Mount Rushmore",			1,	5,	2,	1,	2,	2,	5,	1,	1,	3),
-("Grand Canyon",			1,	5,	2,	2,	4,	2,	5,	1,	1,	1),
-("Vermont",					1,	4,	3,	3,	1,	4,	5,	1,	1,	2),
-("Glacier National Park",	2,	5,	3,	1,	1,	2,	5,	1,	1,	1),
-("Aspen",					1,	3,	3,	3,	3,	3,	5,	1,	1,	1)
+INSERT INTO destinations (destination, themeParks, roadTrip, nightlife, crowds, warm, sports, outdoor, water, cities, historic, photo, blurb)
+VALUES ("New York City",	2,	1,	5,	5,	3,	5,	2,	2,	5,	4, "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQZy0wL_EYYYwQM7jAYw9mg4IgOQUhuMTw4LTzkFfaFn3kvRXqa", ""),
+("New Orleans",				1,	2,	5,	5,	5,	3,	1,	1,	5,	4, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8dHetLV3b20clk8RDhHzR41yVtf6Z3TM9uXkI2UZxavV8hbTFQw", ""),
+("Los Angeles",				4,	1,	5,	5,	5,	4,	1,	3,	4,	2, "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQB3EY5dJv5xYhX9IYcAVq-8PhGZemmSjqK_JqsHLpQQK0wtHyQmg", ""),
+("Washington DC",			2,	1,	4,	5,	3,	2,	2,	1,	5,	5, "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTGiZm5yNOnCo2vLcHemyOzxx8TbjpqqOSHsVZ-IvecD03aztyH", ""),
+("San Francisco",			3,	3,	5,	5,	2,	2,	2,	2,	5,	3, "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTW992n1dUnVFVOS1FfCnSvGGzyzwuYfSbgTu-UCtMwEiQx_Rvu", ""),
+("Las Vegas",				4,	3,	5,	5,	4,	1,	1,	1,	3,	1, "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcROLDiwv4PiPKObHXFJokc2xdnl0fJMJWqu4n5Ga3GNBEzz_Wbp", ""),
+("Oahu",					1,	2,	5,	2,	5,	2,	4,	5,	5,	2, "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRrMJlZ4g0Ire5D8m1LT2sDzmXN-q4GNORj6RsPDp3PETf6DO8feg", ""),
+("Maui",					1,	3,	2,	1,	5,	2,	5,	5,	1,	1, "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQNV4FwWa2hNJ1TG6N9D889917MPP1K5kSTjUxHyiujNzNAD03B", ""),
+("Disney World",					5,	3,	3,	5,	5,	1,	3,	3,	1,	1, "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQ4pT5JQ2F9ZY4Mqe51jmWK4aLzhQhI2RSZPbmQxrXE2IPYtMl_", ""),
+("Florida Keys",			1,	4,	4,	3,	5,	2,	4,	5,	1,	1, "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRGzf7QFvyAxOrFILXDrLr6pLqfzDzqIpOOTXWZL1zPEfKaY8Pq3g", ""),
+("Central Texas",		 	2,	4,	3,	3,	4,	5,	5,	4,	3,	5, "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTOmbU5nUmsw9Mdnfvkq231q9ltzkQgja9ymnNeIjJP-I2sBqOB", ""),
+("Mount Rushmore",			1,	5,	1,	1,	2,	2,	5,	1,	1,	3, "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTOgXmVKhB_xLGJoE9eKYLiEDw_lSPE69EDaCU1BziXsc9bosswJQ", ""),
+("Grand Canyon",			1,	5,	1,	2,	4,	2,	5,	1,	1,	1, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbPb7rEPrlRghMaQ12bTf53x4ECJcwn6UzXVNTGnjtXbA0w2h0cw", ""),
+("Vermont",					1,	4,	1,	3,	1,	4,	5,	1,	1,	2, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWEn-Ck76qq8VVx83434uR3rN7G_V-ELJTLn6vVTaSc-DHmKh-2g", ""),
+("Glacier National Park",	2,	5,	1,	1,	1,	2,	5,	1,	1,	1, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQE3ibz5EYn_6bcqI4ZC7VQu2p5rCaKF6YYoDh5LD9YRAqJFFUN", ""),
+("Aspen",					1,	3,	3,	3,	3,	3,	5,	1,	1,	1, "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRx0z9Q7oyklMU9BpjcPjDYWyXfn11Lx0MiMZnyzJd4BO-m7b1B", "")
 
