2120148d178594d53419450fa110803c12910156
Laura Fiig <Laura Fiig>
2017-01-03 13:00:29 -0600

    html routes working

--- app/routes/html-routes.js
+++ app/routes/html-routes.js
@@ -5,20 +5,25 @@ var path = require("path");
 // Routes
 module.exports = function(app) {
 
-	// Index route loads index.html
+	// Index route 
 	app.get("/", function(req, res) {
-		res.sendFile(path.join(__dirname + "./../public/temp.html"));
+		res.sendFile(path.join(__dirname + "./../public/index.html"));
 	});
 
-	// Index1 route loads index1.html
+	// Survey route loads index1.html
 	app.get("/survey", function(req, res) {
-		res.sendFile(path.join(__dirname + "./../public/temp.html"));
+		res.sendFile(path.join(__dirname + "./../public/survey.html"));
 	});
 
 	// Index1 route loads index1.html
-	app.get("/map", function(req, res) {
-		res.sendFile(path.join(__dirname + "./../public/temp.html"));
+	app.get("/page3", function(req, res) {
+		res.sendFile(path.join(__dirname + "./../public/page3.html"));
 	});  
+
+	// Index1 route loads index1.html
+	app.get("/home", function(req, res) {
+		res.sendFile(path.join(__dirname + "./../public/index.html"));
+	});  	
 
 };
 
