eb15c14b4b63306aa421abae8203b612f96e0971
jstriker84 <jstriker@utexas.edu>
2017-01-04 13:06:04 -0600

    added test file for unit testing

---
+++ Test/test-server.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var mocha = require('mocha'),
+	expect = require('chai').expect;
+
+describe('test suite', function() {
+	it('should test', function() {
+		expect(true).to.be.true;
+	});
+});
