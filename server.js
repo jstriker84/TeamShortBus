edd3695511f95c65d0f24d36d61962fdb8a6cb73
jstriker84 <jstriker@utexas.edu>
2017-01-05 21:51:55 -0600

    edited server.js to prepare for heroku again

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ if (process.env.JAWSDB_URL){
   connection = mysql.createConnection({
     host : "localhost",
     user: "root",
-    password: "root",
+    password: "",
     database: "travelApp_db"
   });
 };
