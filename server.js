0e39b1af67a11c7c68513b420dc97c97b63e6506
jstriker84 <jstriker@utexas.edu>
2017-01-06 08:47:35 -0600

    made it work locally

--- server.js
+++ server.js
@@ -6,12 +6,12 @@
 // =============================================================
 var express = require("express");
 var bodyParser = require("body-parser");
-
+var mysql = require("mysql");
 
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.PORT || 8080;
+var port = 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
